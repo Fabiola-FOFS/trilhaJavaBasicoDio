2fe2704e41f8ac1a8ba8e78cf4aa46978b079c33
Fabiola-FOFS <auproportio@gmail.com>
2025-02-06 14:38:37 -0300

    variaveis e classe string

--- TiposVariaveis/src/TiposVariaveis.java
+++ TiposVariaveis/src/TiposVariaveis.java
@@ -1,5 +1,30 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    
+    //TIPOS PRIMITIVOS //
+    //tipo texto//
+    String meuNome = "Fabiola";
+
+    //tipo numero duplo//
+    double salarioMinimo = 1400;  // não se deve colocar pontos, exceto para centavos ou decimais
+
+    short numeroCurto = 1; //até 30 mil //
+    //int numeroNormal = ;
+    
+    // mudando o valor da variável //
+    int numero = 5; 
+
+    numero = 10;  
+
+    System.out.println(numero); //para devolver o que foi pedido no console
+     
+    //tornando uma variavel num tipo fixo //
+   final double VALOR_DE_PI = 3.14; 
+   // FINAL -- torna a variavel fixa //
+  //TAMBÉM SE ESCREVE O NOME DA VARIAVEL EM CAIXA ALTA//
+    
+  /*estudar : 
+  tipos primitivos , classe string - que representa textos
+
+}
 }
